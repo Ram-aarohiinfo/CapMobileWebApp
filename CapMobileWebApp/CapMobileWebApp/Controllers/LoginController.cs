a4249ab1e084d1c3c0535e8dfea68b09c4cd868f
HarshChaurasia <harshchaurasia789@gmail.com>
2023-05-08 14:12:14 +0530

    uploading project

---
+++ CapMobileWebApp/CapMobileWebApp/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CapMobileWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login()
+        {
+            return Redirect("/home");
+        }
+    }
+}
---
+++ CapMobileWebApp/CapMobileWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CapMobileWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
